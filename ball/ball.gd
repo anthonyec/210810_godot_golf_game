c6c1aaf46760e46188e094b918a3ecd24fdde742
Anthony Cossins <anthonyecossins@gmail.com>
2021-08-13 09:52:42 +0100

    Fix infinite bounces and change bounce signal args

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,9 +1,7 @@
 extends KinematicBody2D
 
 signal stopped_moving
-
-# TODO: Rename dot product var to something that makes more sense, direct_hit-ness?
-signal bounced(speed, normalized_dot_product)
+signal bounced(speed, direction, hit_normal)
 
 # TODO: Rename hitted signal to hit
 signal hitted(power)
@@ -15,20 +13,23 @@ var stop_bias_for_velocity: float = 0.05;
 var velocity: Vector2 = Vector2.ZERO;
 
 func _process(delta):
-	var collision: KinematicCollision2D = move_and_collide(velocity)
-	
-	if collision:
-		velocity = velocity.bounce(collision.normal) * bounce_reduction;
-		emit_signal("bounced", velocity.length(), velocity.normalized().dot(collision.normal))
-
-	velocity *= friction
-	
-	var velocity_is_zero = velocity != Vector2.ZERO
+	var velocity_is_not_zero = velocity != Vector2.ZERO
 	var velocity_mag = velocity.abs().length()
+	var velocity_under_stop_bias = velocity_mag < stop_bias_for_velocity
 	
-	if velocity_is_zero && velocity_mag < stop_bias_for_velocity:
+	if velocity_is_not_zero && velocity_under_stop_bias:
 		velocity = Vector2.ZERO
 		emit_signal("stopped_moving")
+		
+	var collision: KinematicCollision2D = move_and_collide(velocity)
+	
+	## Check for velocity to stop infinite bounces happening when touching a
+	## wall and not moving.
+	if collision && !velocity_under_stop_bias:
+		velocity = velocity.bounce(collision.normal) * bounce_reduction;
+		emit_signal("bounced", velocity.length(), velocity.normalized(), collision.normal)
+
+	velocity *= friction
 
 func hit(direction: Vector2, power: float):
 	velocity += direction * power
--- ball/ball_audio.gd
+++ ball/ball_audio.gd
@@ -8,7 +8,8 @@ func _on_ball_hit(speed: float):
 	ballHitSound.pitch_scale = rand_range(0.8, 1.3)
 	ballHitSound.play()
 
-func _on_ball_bounced(power: float, normalized_dot_product: float):
+func _on_ball_bounced(power: float, direction: Vector2, hit_normal: Vector2):
+	var normalized_dot_product = direction.dot(hit_normal)
 	print("bounced sound ", power, ", ", normalized_dot_product)
 	
 	if normalized_dot_product < 0.2:
